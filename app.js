a19d7a27d495002e038374d6b1da9871d83edd03
Gena Catherine <gena.catharine@gmail.com>
2017-11-15 14:36:17 -0700

    working oauth without env

---
+++ app.js
@@ -0,0 +1,110 @@
+var express = require('express');
+var path = require('path');
+// var dotenv= require('dotenv').config();
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// const CLIENT_ID = process.env.CLIENT_ID
+// const CLIENT_SECRET = process.env.CLIENT_SECRET
+// const PORT = process.env.PORT || 3000;
+// After passport
+const cookieSession = require('cookie-session') // can be anything you want
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// this gives me req.session
+app.use(cookieSession({ secret: 'hellocat' }));
+
+const passport = require('passport')
+// console.log('env', CLIENT_ID, CLIENT_SECRET)
+// A relatively simple data structure that has some urls, and a method or two that are specific to the GitHub API
+const FacebookStrategy = require('passport-facebook').Strategy
+
+// Tells passport to use that facebook-specific data structure
+passport.use(new FacebookStrategy(
+
+  // filling in the blanks on the Facebook strategy
+  {
+    clientID: '567268120293726',
+    clientSecret: 'ddc73d42bd084c53c062e9a6bc6af96c',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    profileFields: ['id', 'displayName', 'photos', 'email'],
+    enableProof: true
+  },
+
+  // after both API calls were made
+    // I've processed the initial login of the user
+    //this was different on facebook passport github directions
+    function onSuccessfulLogin(token, refreshToken, profile, done) {
+// I've processed the initial login of the user
+// This happens once
+    done(null, {token, profile});
+  }
+))
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// take in whatever was passed into `done` inside the FacebookStrategy config
+passport.serializeUser((object, done) => {
+  console.log("Serialize User", {token: object})
+
+  // when I call `done` _here_, I am passing in the data to be saved to the session
+  done(null, {token: object.token})
+})
+
+passport.deserializeUser((object, done) => {
+  console.log("Deserialize User", object)
+  done(null, object)
+})
+
+
+app.get('/auth/facebook', passport.authenticate('facebook'
+, { scope: ['user_friends', 'manage_pages'] }));
+
+// makes 2 api calls to facebook
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }), ()=> {
+    console.log('made it to callback')
+  });
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
